31c21b53f3876d9e48531619f435f84eed962100
naman79 <naman79@naver.com>
2026-01-05 17:54:05 +0900

    Adds SQL queries for data analysis
    Adds SQL queries to investigate data related to Gangwon-do integrated public funds, including account information, fund operation accounts, and balance details.
    Includes queries to examine data for specific periods and identify data related to a particular process (8620).
    Also adds supporting excel and image files related to the data analysis.

--- 20260105.sql
+++ 20260105.sql
@@ -1,6 +1,46 @@
+-- 강원도 통합공금 계좌 정보 확인
 select 
  * 
 from ACL_SIGUMGO_MAS
 where MNG_NO = 1
 and sigumgo_org_c = 42 
 and LINK_ACSER = '125420'
+
+-- 강원도 통합공금
+-- 04200080900003999
+
+-- 강원도 통합공금 자금운용계좌 정보 확인
+-- unyong_gyejwa = '160000125420'
+
+
+select 
+ *
+from RPT_UNYONG_GYEJWA
+where GEUMGO_CODE = 42
+and unyong_gyejwa = '160000125420'
+
+-- 강원도 통합공금 자금운용계좌 잔액정보 확인
+
+select 
+ *
+from RPT_UNYONG_JAN
+where GEUMGO_CODE = 42
+and unyong_gyejwa = '160000125420'
+
+
+select 
+ *
+from RPT_UNYONG_JAN
+where GEUMGO_CODE = 42
+and unyong_gyejwa = '160000125420'
+and kijunil like '2025%'
+order by kijunil
+
+
+-- 8620 이월 데이터 확인하기
+
+select 
+ *
+from ACL_SIGUMGO_SLV
+where trxdt like '2026%'
+and lst_chg_scr_id like '%8620%'
